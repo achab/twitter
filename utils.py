1d35c72ba5f309a5e1145ae21153689fe39c055d
Massil <massil.achab@gmail.com>
2015-11-24 19:00:37 +0100

    ...

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 import tweepy
+import json
 from authorize import give_api
 
 api = give_api()
 
-def print_last_status(n=10):
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+
+def process_last_status(n=10):
     for status in tweepy.Cursor(api.home_timeline).items(n):
         # Process a single status
-        print(status.text)
+        process_or_store(status._json)
 
